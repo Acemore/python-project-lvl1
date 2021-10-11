e0020fb1a4fd3a063616037d92aaa499a8e0df78
Sergey Martynov <acemore@mail.ru>
2021-10-11 16:25:08 +0300

    Update module brain_games_engine.py

--- brain_games/brain_games_engine.py
+++ brain_games/brain_games_engine.py
@@ -1,20 +1,26 @@
 import prompt
 
-def run_game(game_descrition, data_in, correct_answer):
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
-    
-    print(game_descrition)
 
-    for _ in range(3):
-        print('Question: {}'.format(data_in))
-        user_input = prompt.string('Your answer: ')
-        
-        if user_input == str(correct_answer):
+def run_game(game_rules, data_in, correct_answer):
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print('Hello, {}!'.format(user_name))
+
+    print(game_rules)
+
+    for i in range(3):
+        print('Question: {}'.format(data_in[i]))
+        user_answer = prompt.string('Your answer: ')
+
+        if user_answer == str(correct_answer[i]):
             print('Correct!')
         else:
-            print('{} is wrong answer ;(. Correct answer was {}.'.format(user_input, correct_answer))
-            break
+            print(
+                "'{}' is wrong answer ;(. Correct answer was '{}'."
+                .format(user_answer, correct_answer[i])
+            )            
+            print("Let's try again, {}!".format(user_name))
+            
+            exit()
 
-    print('Congratulations, {}!'.format(name))
+    print('Congratulations, {}!'.format(user_name))
