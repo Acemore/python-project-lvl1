f6f6803c7462c70645e2f748f5947210706f2ce9
Sergey Martynov <acemore@mail.ru>
2021-10-12 19:47:22 +0300

    Update module brain_game_even.py

--- brain_games/games/brain_game_even.py
+++ brain_games/games/brain_game_even.py
@@ -1,11 +1,12 @@
 from random import randint
+from brain_games import rounds_count
 from brain_games.brain_games_engine import run_game
 
 
 def get_random_numbers():
     random_numbers = []
 
-    [random_numbers.append(randint(0, 100)) for _ in range(3)]
+    [random_numbers.append(randint(0, 100)) for _ in range(rounds_count)]
 
     return random_numbers
 
@@ -13,7 +14,7 @@ def get_random_numbers():
 def is_even(random_numbers_list):
     is_even_list = []
 
-    for i in range(3):
+    for i in range(rounds_count):
         if random_numbers_list[i] % 2 == 0:
             is_even_list.append('yes')
         else:
@@ -25,5 +26,6 @@ def is_even(random_numbers_list):
 def play_game_even():
     game_rules = 'Answer "yes" if the number is even, otherwise answer "no".'
     random_numbers = get_random_numbers()
+    correct_answers = is_even(random_numbers)
 
-    run_game(game_rules, random_numbers, is_even(random_numbers))
+    run_game(game_rules, random_numbers, correct_answers)
