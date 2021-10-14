882ca1fb55335bae8ca1cb3de5952251bb286de4
Sergey Martynov <acemore@mail.ru>
2021-10-14 13:28:11 +0300

    Add script brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from brain_games.games.brain_game_progression import play_game_progression
+
+
+def main():
+    play_game_progression()
+
+
+if __name__ == '__main__':
+    main()
