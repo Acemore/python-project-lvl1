2d59d138b4d0f0085369c3628db38b0f94cc43e0
Sergey Martynov <acemore@mail.ru>
2021-10-15 23:00:07 +0300

    Add script brain_prime.py

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games.brain_games_engine import run_game
+import brain_games.games.brain_game_prime as brain_game_prime
+
+
+def main():
+    run_game(brain_game_prime)
+
+
+if __name__ == '__main__':
+    main()
